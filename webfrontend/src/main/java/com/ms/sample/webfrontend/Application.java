f1c1b901181f90126c144509d2276410507e29d8
Yuanbin Teng <yute@microsoft.com>
2018-12-12 15:06:17 +0800

    'init'

---
+++ webfrontend/src/main/java/com/ms/sample/webfrontend/Application.java
@@ -0,0 +1,18 @@
+package com.ms.sample.webfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/greeting", produces = "text/plain")
+    public String greeting() {
+        return "Hello from webfrontend";
+    }
+}
