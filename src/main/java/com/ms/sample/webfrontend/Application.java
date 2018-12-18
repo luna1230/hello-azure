398459fc5c75ebd9423b2066746097356a4511b3
Yuanbin Teng <yute@microsoft.com>
2018-12-18 11:07:18 +0800

    'modify'

--- src/main/java/com/ms/sample/webfrontend/Application.java
+++ src/main/java/com/ms/sample/webfrontend/Application.java
@@ -3,7 +3,8 @@ package com.ms.sample.webfrontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-
+import java.io.*;
+import java.net.*;
 @SpringBootApplication
 @RestController
 public class Application {
@@ -12,7 +13,12 @@ public class Application {
     }
 
     @RequestMapping(value = "/greeting", produces = "text/plain")
-    public String greeting() {
-        return "Hello from webfrontend";
+    public String greeting(@RequestHeader(value = "azds-route-as", required = false) String azdsRouteAs) throws Exception {
+        URLConnection conn = new URL("http://mywebapi/").openConnection();
+        conn.setRequestProperty("azds-route-as", azdsRouteAs); // propagate dev space routing header
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream())))
+        {
+            return "Hello from webfrontend and " + reader.lines().reduce("\n", String::concat);
+        }
     }
 }
